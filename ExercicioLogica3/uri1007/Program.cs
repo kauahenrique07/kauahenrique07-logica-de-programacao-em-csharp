23b035864bb938c07d0d3bb510d19964668bc700
Kauã Henrique <90483262+kauahenrique07@users.noreply.github.com>
2023-03-04 15:30:46 -0300

    Add files via upload

---
+++ ExercicioLogica3/uri1007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1001 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o primeiro número: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("Digite o terceiro número: ");
+            int C = int.Parse(Console.ReadLine());
+            Console.Write("Digite o quarto número: ");
+            int D = int.Parse(Console.ReadLine());
+
+            int diferenca = (A * B - C * D);
+
+            Console.WriteLine("\nDIFERENCA = " + diferenca);
+        }
+    }
+}
