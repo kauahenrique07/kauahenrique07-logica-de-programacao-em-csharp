04df560ad4948046c8d510df3a20071927cec7d4
Kauã Henrique <90483262+kauahenrique07@users.noreply.github.com>
2023-03-04 15:29:21 -0300

    Add files via upload

---
+++ ExercicioLogica2/uri1002/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioLogica2 {
+    class Program {
+        static void Main(string[] args) {
+
+            Double raio = double.Parse(Console.ReadLine());
+            Double pi = 3.14159;
+
+            Double area = pi * Math.Pow(raio, 2.0);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
